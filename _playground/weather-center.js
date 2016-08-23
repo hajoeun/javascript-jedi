6532bde2348381c4f41826b583bf08ed11a3fb54
Joeun <imjoeunha@gmail.com>
2016-08-23 20:40:07 +0900

    _playground: show-the-local-weather
    Show the Local Weather 프로젝트 개선
    1. 비동기로 호출되는 API를 처리하는 방법 변경 (타이머 -> 콜백 함수 안에서 호출)

--- _playground/weather-center.js
+++ _playground/weather-center.js
@@ -1,19 +1,16 @@
 var weatherCenter = (function() {
-  var weatherData = {}, lat, lon;
+  var weatherData = {}, lat, lon, key = '226348949859717c36bf5cea47b691bc';
 
   function WeatherCenter() {
-    // 구글 지오로케이션 api로부터 지역 정보(위치)를 가져오는 메소드, 순서 1번
-    this.getLocation = function() {
-      $.post("https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyA_3mlUgaVTJwbitAvE3T5V-szKuIcAfC4", function(position) {
-        lat = position.location.lat;
-        lon = position.location.lng;
-      });
+    // 데이터를 화면에 출력해주는 메소드, 순서 3번
+    var showWeather = function() {
+      $("#data").html(weatherData.location + "<br>" +
+      weatherData.temp + weatherData.tempFlag + "<br>" +
+      weatherData.main + "<img src='http://openweathermap.org/img/w/" + weatherData.icon + ".png'>");
     };
 
     // openweathermap api로부터 기상 정보(날씨)를 가져오는 메소드, 순서 2번
-    this.getWeather = function () {
-      var key = '226348949859717c36bf5cea47b691bc';
-      var weatherJson = "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon  + "&APPID=" + key;
+    var getWeather = function (weatherJson) {
       $.getJSON(weatherJson, function(obj) {
         weatherData.main = obj.weather[0].main;
         weatherData.icon = obj.weather[0].icon;
@@ -21,7 +18,17 @@ var weatherCenter = (function() {
         if (!weatherData.temp) {
           weatherData.temp = (obj.main.temp - 273.15).toFixed(1);
           weatherData.tempFlag = "°C";
+          showWeather();
         }
+      });
+    };
+
+    // 구글 지오로케이션 api로부터 지역 정보(위치)를 가져오는 메소드, 순서 1번
+    this.getLocation = function() {
+      $.post("https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyA_3mlUgaVTJwbitAvE3T5V-szKuIcAfC4", function(position) {
+        lat = position.location.lat;
+        lon = position.location.lng;
+        getWeather("http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon  + "&APPID=" + key);
       });
     };
 
@@ -34,30 +41,17 @@ var weatherCenter = (function() {
         weatherData.temp = ((weatherData.temp - 32) / 1.8).toFixed(1);
         weatherData.tempFlag = "°C";
       }
-      this.showWeather();
-    };
-
-    // 데이터를 화면에 출력해주는 메소드, 순서 3번
-    this.showWeather = function() {
-      $("#data").html(weatherData.location + "<br>" +
-      weatherData.temp + weatherData.tempFlag + "<br>" +
-      weatherData.main + "<img src='http://openweathermap.org/img/w/" + weatherData.icon + ".png'>");
+      showWeather();
     };
   }
   return new WeatherCenter();
 })();
 
-
 if (navigator.geolocation) {
-  setTimeout(weatherCenter.getLocation,10);
-  setTimeout(weatherCenter.getWeather,500);
-  setTimeout(weatherCenter.showWeather,1000);
+  weatherCenter.getLocation();
 }
 
 $(document).ready(function() {
-  $('#title').on('click', function() {
-    weatherCenter.showWeather();
-  });
   $('#data').on('click', function() {
     weatherCenter.toggleMeasurement();
   });
