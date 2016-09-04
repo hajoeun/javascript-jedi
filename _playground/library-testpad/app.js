8f75ffa75c0d1d7e508c44a41155c981c8e3c00a
Joeun <imjoeunha@gmail.com>
2016-09-04 23:05:49 +0900

    _playground: library-testpad 기능 개선
    - 에러 핸들링를 위한 코드 추가
    - console.log() 함수 및 << >> 사용 방식 변경

--- _playground/library-testpad/app.js
+++ _playground/library-testpad/app.js
@@ -1,17 +1,21 @@
 
   var logpad = document.getElementById('log'),
-
-  console = {
-    log: function(str) {
+      log = function(str) {
       logpad.innerHTML += str + "<br>";
-    }
-  };
+      };
 
   function runCode(value) {
     logpad.innerText = "";
-    var code = value.replace(/[\n]/g, '').replace(/<</g,"console.log(").replace(/;?>>/g,");");
-    eval(code);
+    var code = value.replace(/[\n]/g, '').replace(/console./g, "")
+                    .replace(/<</g,"log(\"").replace(/;?>>/g,"\");"); // 테스트의 편의를 위해 console.log 함수 대신 로그를 띄워주는 기능 추가 쌍꺽세 <<>> 안의 문자열을 바로 로그를 남김
 
+    try {
+      eval(code);
+    } catch (e) {
+      log("<error>" + e + "</error>");
+    }
+
+    // alert의 경우 외부 창에서 키가 기록되면서 발생하는 문제가 있어서 이를 해결하기 위해서 또 삭제를 해줌
     delete keys[13];
     delete keys[17];
   }
