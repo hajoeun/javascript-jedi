429dda6847e3bfbb5c33eb4faf4216e8976849ea
Joeun <imjoeunha@gmail.com>
2016-10-02 23:38:53 +0900

    _playground: library-testpad
    - 소소한 변경 사항 및 주석 추가

--- _playground/library-testpad/app.js
+++ _playground/library-testpad/app.js
@@ -1,16 +1,18 @@
 
   var logpad = document.getElementById('log'),
       log = function(str) {
-      logpad.innerHTML += str + "<br>";
+        // if (typeof str !== 'string') str = str.toString();
+        // console.log(typeof str.toString()); // 객체나 배열도 출력되도록 수정해보자 그리고 함수만 실행했을 때 반환값도 출력해주자, 호이스팅 문제도 해결해야할듯...
+        logpad.innerHTML += str + "<br>";
       };
 
   function runCode(value) {
     logpad.innerText = "";
-    var code = value.replace(/[\n]/g, '').replace(/console./g, "")
-                    .replace(/<</g,"log(\"").replace(/;?>>/g,"\");"); // 테스트의 편의를 위해 console.log 함수 대신 로그를 띄워주는 기능 추가 쌍꺽세 <<>> 안의 문자열을 바로 로그를 남김
+    var code = value.replace(/\/\/.+\n?/g, '').replace(/\n/g, '') // 주석 제거 줄바꿈 제거
+    .replace(/console./g, '').replace(/<</g,"log(").replace(/;?>>/g,");"); // 테스트의 편의를 위해 console.log 함수 대신 로그를 띄워주는 기능 추가 쌍꺽세 <<>> 안의 문자열을 바로 로그를 남김
 
     try {
-      eval(code);
+      setTimeout(code, 0);
     } catch (e) {
       log("<error>" + e + "</error>");
     }
